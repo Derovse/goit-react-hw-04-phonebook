bdb782c39b659e9b3346e187c071482e69882be3
Derovse <sashamartunyk2004@gmail.com>
2023-08-11 18:03:13 +0300

    в
    в

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
 import ContactForm from './ContactForm/ContactForm';
 import ContactList from './ContactList/ContactList';
 import Filter from './Filther/Filther';
